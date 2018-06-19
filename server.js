636aff1a0e801e6a92536e6a2e2b7ce138cf1c6b
Keren Shimoni <kerenshimoni6@gmail.com>
2018-06-19 14:25:03 -0700

    organize file structure

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+
+
+// Define middleware, body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -13,6 +22,14 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+//use routes
+app.use(routes);
+
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
